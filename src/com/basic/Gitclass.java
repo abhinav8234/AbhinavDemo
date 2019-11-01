36b838bb40be3b42202d6f856e1cd4d5ad9d022f
abhinav <abhinav8234@gmail.com>
2019-11-01 23:42:49 +0530

    hii

--- src/com/basic/Gitclass.java
+++ src/com/basic/Gitclass.java
@@ -1,5 +1,11 @@
 package com.basic;
 
+import java.nio.charset.MalformedInputException;
+
 public class Gitclass {
 
+	public static void main(String[] args) {
+		
+		System.out.println("hi abhinav");
+	}
 }
